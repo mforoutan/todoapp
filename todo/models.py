41f9625a612128f04a3b8007c95a61497c7ae4d0
Foroutan <foroutan.dev@gmail.com>
2021-10-17 20:39:29 +0330

    Added Task and List models

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class Task(models.Model):
+    text = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+
+class List(models.Model):
+    title = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    tasks = models.ForeignKey(Task, on_delete=CASCADE)
