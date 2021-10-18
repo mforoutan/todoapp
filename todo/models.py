94a419e528669c5f1fe833c6e01c6f5ab985fabb
Foroutan <foroutan.dev@gmail.com>
2021-10-18 15:12:13 +0330

    Minor Fixes

--- todo/models.py
+++ todo/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 
 
+class List(models.Model):
+    title = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    # def save(self):
+    #     if self.title == "":
+    #         self.title = "List #{}".format(self.id)
+
+    def __str__(self):
+        return self.title
+
 class Task(models.Model):
     text = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
-
-
-class List(models.Model):
-    title = models.CharField(max_length=200)
-    created_on = models.DateTimeField(auto_now_add=True)
-    tasks = models.ForeignKey(Task, on_delete=CASCADE)
+    list = models.ForeignKey(List, on_delete=CASCADE, default=1)
