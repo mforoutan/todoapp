41a5396980f16a9dd988402cbb68f0a12ba8e29c
Foroutan <foroutan.dev@gmail.com>
2021-10-22 17:13:52 +0330

    Deleted List model and changed Task model

--- todo/models.py
+++ todo/models.py
@@ -1,20 +1,13 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
-
-class List(models.Model):
-    title = models.CharField(max_length=200)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    # def save(self):
-    #     if self.title == "":
-    #         self.title = "List #{}".format(self.id)
-
-    def __str__(self):
-        return self.title
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
     text = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
-    list = models.ForeignKey(List, on_delete=CASCADE, default=1)
+
+    def __str__(self):
+        return "Task: {} from User: {}".format(self.text, self.user.username)
