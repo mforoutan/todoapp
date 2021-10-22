8cad805f19fde228b0a8b7955934855e91b179fb
Foroutan <foroutan.dev@gmail.com>
2021-10-22 17:16:18 +0330

    Added taskModelAdmin for Task model registeration

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Task, List
+from django.contrib.admin.decorators import action
+from django.contrib.admin.options import ModelAdmin
+from .models import Task
 
 
-admin.site.register(Task)
-admin.site.register(List)
+@admin.action(description="Mark tasks as complete.")
+def complete_tasks(modeladmin, request, queryset):
+    queryset.update(status=True)
+
+class taskModelAdmin(ModelAdmin):
+    list_display = ['text', 'user', 'created_on', 'status']
+    actions = [complete_tasks]
+
+admin.site.register(Task, taskModelAdmin)
