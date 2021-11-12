d12e9a000784ed7beef7b129b2e44a7c16f3caa9
Foroutan <foroutan.dev@gmail.com>
2021-11-12 18:33:31 +0330

    Added STATICFILES_DIRS and Template DIR.

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'todoapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 
 STATIC_URL = '/assets/'
 
