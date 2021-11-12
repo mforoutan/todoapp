9c8c24c37f17f6ba2dafdf2d1967df63505492d0
Foroutan <foroutan.dev@gmail.com>
2021-11-12 18:34:03 +0330

    Added Homepage(index) to the project

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def indexView(request):
+    if request.user.is_authenticated :
+        tasks = Task.objects.filter(user=request.user)
+    template = "../templates/index.html"
+    context = {
+        'tasks': tasks,
+    }
+    return render(request,template,context)
