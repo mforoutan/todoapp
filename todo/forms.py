2fa3dfd230f6a8cac360f3a609dc097385ed182b
Foroutan <66463596+mforoutan@users.noreply.github.com>
2022-01-07 19:04:26 +0330

    Added login and signup forms

--- todo/forms.py
+++ todo/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms import widgets
+from django.forms.fields import CharField
+from django.forms.widgets import PasswordInput, TextInput
 from .models import Task
 
 
@@ -13,3 +16,31 @@ class TaskForm(forms.ModelForm):
                 'placeholder': 'Add your new todo.',
                 }),
         }
+
+
+class LoginForm(forms.Form):
+    username = CharField(max_length=200, widget=TextInput(attrs={
+        'Placeholder': 'Username'
+    }))
+    password = CharField(widget=PasswordInput(attrs={
+        'placeholder': 'Password'
+    }))
+
+
+class SignupForm(forms.Form):
+    username = CharField(max_length=200, widget=TextInput(attrs={
+        'Placeholder': 'Username',
+        'id': 'username',
+    }))
+    password = CharField(widget=PasswordInput(attrs={
+        'Placeholder': 'Password',
+        'id': 'password',
+    }))
+    confirm_password = CharField(widget=PasswordInput(attrs={
+        'Placeholder': 'Confirm Password',
+        'id': 'confirm_password',
+    }))
+    email = CharField(max_length=200, widget=TextInput(attrs={
+        'Placeholder': 'Email',
+        'id': 'email',
+    }))
